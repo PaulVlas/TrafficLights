e1285e0875dcbf925a6b52748e7381a31b31d997
PaulVlas <paul.vlaswinkel@gmail.com>
2025-04-01 18:46:59 +0200

    Make traffic light blink

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { TrafficLight } from "./components/TrafficLight/TrafficLight";
 function App() {
   return (
     <div className="App">
-      <TrafficLight isActive={true} />
+      <TrafficLight />
     </div>
   );
 }
--- src/components/TrafficLight/TrafficLight.jsx
+++ src/components/TrafficLight/TrafficLight.jsx
@@ -1,14 +1,30 @@
 import { Light } from "../Light/Light";
+import { useState, useEffect } from "react";
 import style from "./style.module.css";
 
 const TRAFFIC_LIGHT_STATES = ["red", "orange", "green"];
+const INTERVAL = 1200;
 
-export const TrafficLight = ({ color }) => {
+export const TrafficLight = () => {
+  const [currentColor, setCurrentColor] = useState(TRAFFIC_LIGHT_STATES[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentColor((prevColor) => {
+        const currentIndex = TRAFFIC_LIGHT_STATES.indexOf(prevColor);
+        const nextIndex = (currentIndex + 1) % TRAFFIC_LIGHT_STATES.length;
+        return TRAFFIC_LIGHT_STATES[nextIndex];
+      });
+    }, INTERVAL);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={style.wrapper}>
       <div className={style.trafficlight}>
         {TRAFFIC_LIGHT_STATES.map((c) => (
-          <Light key={c} color={c} active={c === color} />
+          <Light key={c} color={c} active={c === currentColor} />
         ))}
       </div>
     </div>
